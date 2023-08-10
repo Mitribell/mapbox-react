8601c1d7e3464924f30c0d8d140054e24adc2e93
Dmytro <Mitribell@gmail.com>
2023-08-10 15:08:06 +0300

    12

--- src/App.js
+++ src/App.js
@@ -18,10 +18,19 @@ export default function App() {
           center: [lng, lat],
           zoom: zoom
         });
-      });
+        
+        map.current.on('move', () => {
+          setLng(map.current.getCenter().lng.toFixed(4));
+          setLat(map.current.getCenter().lat.toFixed(4));
+          setZoom(map.current.getZoom().toFixed(2));
+        });
+      });     
 
       return (
         <div>
+          <div className="sidebar">
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+          </div>
           <div ref={mapContainer} className="map-container" />
         </div>
       );
